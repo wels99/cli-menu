0c11a481e14b0d1da86f2430136323ee7ebd6832
djf <djf@lc.com>
2024-03-30 15:05:10 +0800

    清理代码

--- color.go
+++ color.go
@@ -1,15 +1,6 @@
 package climenu
 
 const (
-	Color_min = 30 + iota
-	Color_Red
-	Color_Green
-	Color_Yellow
-	Color_Blue
-	Color_max
-)
-
-const (
 	Style_Reverse   = "\033[7m"
 	Style_Black     = "\033[30;1m"
 	Style_Red       = "\033[31;1m"
